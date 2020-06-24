9955574f5b5c006fdf34b512595df265b99d67a4
Jonas Wanke <contact@jonas-wanke.com>
2020-06-24 07:19:51 +0200

    feat(libc): add bit, bitfield operations
    Co-authored-by: Marcel Garus <marcel.garus@gmail.com>
    Co-authored-by: Rohan Sawahn <rohan.sawahn@hotmail.de>

--- libc/include/sys/types.h
+++ libc/include/sys/types.h
@@ -7,16 +7,39 @@
 
 typedef unsigned int size_t;
 
-typedef unsigned char 		uint8_t;
-typedef unsigned short		uint16_t;
-typedef unsigned int 		uint32_t;
-typedef unsigned long long 	uint64_t;
+typedef unsigned char       uint8_t;
+typedef unsigned short      uint16_t;
+typedef unsigned int        uint32_t;
+typedef unsigned long long  uint64_t;
 
-typedef char 		int8_t;
-typedef short		int16_t;
-typedef int 		int32_t;
-typedef long long 	int64_t;
+typedef char      int8_t;
+typedef short     int16_t;
+typedef int       int32_t;
+typedef long long int64_t;
 
 #define false 0
 #define true 1
 typedef uint8_t bool;
+
+// Endianness
+
+#define UINT32_ENDIANNESS_SWAP(value) \
+    (((value) << 24 & 0xff000000) | ((value) << 8 & 0x00ff0000) \
+      | ((value) >> 8 & 0x0000ff00) | ((value) >> 24 & 0x000000ff))
+
+
+// Bit & Bit Field operations
+
+#define BIT_SET(value, nbit)   ((value) |=  (1 << (nbit)))
+#define BIT_CLEAR(value, nbit) ((value) &= ~(1 << (nbit)))
+#define BIT_FLIP(value, nbit)  ((value) ^=  (1 << (nbit)))
+#define BIT_CHECK(value, nbit) ((value) &   (1 << (nbit)))
+
+// Produces [length] consecutive 1's.
+#define BITS_ONE(length)                    ((1 << (length)) - 1)
+#define BITFIELD_GET(value, offset, length) (((value) >> (offset)) & BITS_ONE(length))
+#define BITFIELD_MERGE(old_value, new_bits, new_offset, new_length) \
+    (((old_value) & (~(BITS_ONE(new_length) << (new_offset)))) \
+        | (((new_bits) & BITS_ONE(new_length)) << (new_offset)))
+#define BITFIELD_SET(field, value, offset, length) \
+    ((field) = BITFIELD_MERGE(field, value, offset, length))
